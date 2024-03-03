577d33d8879b8ec04a1a504f49030d74922adab3
黄柏特 <471314513@qq.com>
2024-03-03 17:08:41 +0800

    log:add nn_convolution swanlog

--- hello pytorch/lesson/lesson-12/nn_layers_convolution.py
+++ hello pytorch/lesson/lesson-12/nn_layers_convolution.py
@@ -6,6 +6,7 @@
 # @brief      : 学习卷积层
 """
 import os
+import swanlab
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 import torch.nn as nn
 from PIL import Image
@@ -20,7 +21,8 @@ sys.path.append(hello_pytorch_DIR)
 
 from tools.common_tools import transform_invert, set_seed
 
-set_seed(3)  # 设置随机种子
+set_seed(4)  # 设置随机种子
+swanlab.init(experiment_name="convolution_layers")
 
 # ================================= load img ==================================
 path_img = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lena.png")
@@ -34,8 +36,8 @@ img_tensor.unsqueeze_(dim=0)    # C*H*W to B*C*H*W
 # ================================= create convolution layer ==================================
 
 # ================ 2d
-flag = 1
-# flag = 0
+#flag = 1
+flag = 0
 if flag:
     conv_layer = nn.Conv2d(3, 1, 3)   # input:(i, o, size) weights:(o, i , h, w)
     nn.init.xavier_normal_(conv_layer.weight.data)
@@ -44,8 +46,8 @@ if flag:
     img_conv = conv_layer(img_tensor)
 
 # ================ transposed
-# flag = 1
-flag = 0
+flag = 1
+#flag = 0
 if flag:
     conv_layer = nn.ConvTranspose2d(3, 1, 3, stride=2)   # input:(i, o, size)
     nn.init.xavier_normal_(conv_layer.weight.data)
@@ -60,6 +62,7 @@ img_conv = transform_invert(img_conv[0, 0:1, ...], img_transform)
 img_raw = transform_invert(img_tensor.squeeze(), img_transform)
 plt.subplot(122).imshow(img_conv, cmap='gray')
 plt.subplot(121).imshow(img_raw)
+swanlab.log({"transposed_convolution_photo":swanlab.Image(plt)})
 plt.show()
 
 
